d74cffbd09a7fa969948c85caed1fb0419e2183f
Nguyen Minh Chanh <nguyenminhchanh1910@gmail.com>
2022-10-02 22:16:22 +0700

    submit

---
+++ src/main/java/com/example/th_tuan4/ThTuan4Application.java
@@ -0,0 +1,13 @@
+package com.example.th_tuan4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThTuan4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThTuan4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/th_tuan4/ThTuan4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.th_tuan4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThTuan4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
